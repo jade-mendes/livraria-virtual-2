a133bca8298ec6a82bf5656c652c5150053965ad
Jade <jdmsa3@gmail.com>
2025-03-27 20:45:39 -0300

    feat: colocando os server components na Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Banner  from "../app/components/Banner";
+import BookList from "./components/BookList/BookList";
 import './globals.css'
 
 export default function Home(){
     return(
         <>
-            <Banner />
+            <Banner/>
+            <BookList/>
         </>
     )
 }
